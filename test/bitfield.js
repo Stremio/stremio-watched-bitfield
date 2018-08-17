56326610a64f16e48d9ee9e5a22f5336c96e1dea
Ivo Georgiev <ivo@strem.io>
2018-08-17 16:52:54 +0300

    bitfield test added

---
+++ test/bitfield.js
@@ -0,0 +1,16 @@
+var BitField8 = require('../lib/bitfield8')
+var tape = require('tape')
+
+// @TODO: if bitfield is made private, make separate BitField8 test
+
+tape('construct and set', function(t) {
+	var f = new BitField8(8)
+
+	t.ok(f, 'has bitfield')
+
+	for (var i=3; i!=6; i++) f.set(i, true)
+
+	for (var i=0; i!=f.length; i++) t.equal(f.get(i), i >= 3 && i<6, 'is right val')
+
+	t.end()
+})
---
+++ test/watchedBitfield.js
@@ -0,0 +1,31 @@
+var watchedBitfield = require('../')
+var tape = require('tape')
+
+tape('construct from array', function(t) {
+	var bitArray = [  0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 ]
+	var ids =      [ '1','2','3','4','5','6','7','8','9','a','b' ]
+
+	var wb = watchedBitfield.constructFromArray(bitArray, ids)
+	
+	t.deepEquals(wb.videoIds, ids, 'videoIds equal')
+	t.deepEquals(range(0, bitArray.length).map(idx => wb.bitfield.get(idx) ? 1 : 0), bitArray, 'bitfield is equal')
+
+	// @TODO: serialize
+
+	t.end()
+})
+
+tape('construct and resize: appended objects', function(t) {
+	t.end()
+})
+
+tape('construct and resize: append at the end, remove from the beginning', function(t) {
+	t.end()
+})
+
+// helpers lol
+function range(a, b) { var ar = []; for (var i=a; i!=b; i++) ar.push(i); return ar }
+
+// if (Array.isArray(state.watchedEpisodes) && meta.videos.length === state.watchedEpisodes.length) {
+// 	watchedBitfield.constructFromArray(state.watchedEpisodes, meta.videos.map(function(v) { return v._id }))
+// }
