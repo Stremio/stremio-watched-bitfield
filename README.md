98ee7c01ffb5aaa00736de85cbd990cf6d049821
Ivo Georgiev <ivo@strem.io>
2018-08-17 17:16:48 +0300

    more testing, working on watchedBitfield serialization/deserialization

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 `watchedBitfield.constructAndResize(serialized, videoIds)`
 
 `watchedBitfield.constructFromArray(arr, videoIds)`
+
+Always use the last two functions in `try..catch`, because they will throw with invalid data
--- test/watchedBitfield.js
+++ test/watchedBitfield.js
@@ -1,16 +1,44 @@
 var watchedBitfield = require('../')
 var tape = require('tape')
 
-tape('construct from array', function(t) {
-	var bitArray = [  0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 ]
-	var ids =      [ '1','2','3','4','5','6','7','8','9','a','b' ]
+tape('construct from array, serialize/deserialize', function(t) {
+	var bitArray = [  0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , 1  ]
+	var ids =      [ '1','2','3','4','5','6','7','8','9','a','b','c' ]
 
 	var wb = watchedBitfield.constructFromArray(bitArray, ids)
 	
 	t.deepEquals(wb.videoIds, ids, 'videoIds equal')
 	t.deepEquals(range(0, bitArray.length).map(idx => wb.bitfield.get(idx) ? 1 : 0), bitArray, 'bitfield is equal')
 
-	// @TODO: serialize
+	var serialized = wb.serialize()
+
+	console.log(serialized)
+
+	//console.log(watchedBitfield.constructAndResize(serialized, ids))
+
+	t.end()
+})
+
+tape('keeps big arrays small in serialized size', function(t) {
+	var bitArray = new Array(500)
+
+	for (var i=0; i!=500; i++) bitArray[i] = 0
+
+	// user watched first 40 and then some other 3, then another 4
+	for (var i=0; i!=40; i++) bitArray[i] = 1
+	for (var i=50; i!=53; i++) bitArray[i] = 1
+	for (var i=400; i!=404; i++) bitArray[i] = 1
+
+	var ids = bitArray.map(function(x, i) { return 'vid'+i })
+	var wb = watchedBitfield.constructFromArray(bitArray, ids)
+
+	var serialized = wb.serialize()
+
+	console.log(serialized)
+
+	t.ok(serialized.length < 50, 'under 50 characters')
+
+	// @TODO: can recover it
 
 	t.end()
 })
